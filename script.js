9bcc6ebba432ecba845a1d1b45e9b19962cf5b26
Sean Duiser <duzer84@gmail.com>
2022-02-28 16:53:02 -0500

    program generates random characters based on user input. Need to jumble resulting string.

--- script.js
+++ script.js
@@ -1,27 +1,57 @@
 // Assignment Code
+
+var letters = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numbers = "0123456789"
+var special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~\"\\"
+var genPassword = "";
 var generateBtn = document.querySelector("#generate");
 
+// user enters how many characters they'd like their PW to be
 function generatePassword() {
   var count = Number(prompt("Please enter the amount of characters you would like in your password."));
   if (count < 8 || count > 128) {
-    console.log("Out of range")
-
+    console.log("Out of range");
+    alert('Please select a number between 8 and 128.');
     return "";
   }
-  var letters = "abcdefghijklmnopqrstuvwxyz";
-  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  var numbers = "0123456789"
-  var special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~\"\\"
-  var genPassword = "";
 
-  for (var i =0; i < count; i++) {
+  // user will be given a password of only random lower-case letters if nothing else is selected
+  for (var i = 0; i < count; i++) {
     var random = Math.floor(Math.random() * letters.length);
     genPassword += letters[random];
+  }
+
+// user is able to confirm whether or not they'd like upper-case letters in their PW
+  var upper = Boolean(confirm("Would you like your password to contain upper-case letters?"));
+  if (upper) {
+    for (var i = 0; i < count; i++) {
+      var random = Math.floor(Math.random() * uppercase.length);
+      genPassword += uppercase[random];
+    }
+  }
+
+  // user is able to confirm whether or not they'd like numbers in their PW
+  var numeric = Boolean(confirm("Would you like your password to contain numbers?"));
+  if (numeric) {
+    for (var i = 0; i < count; i++) {
+      var random = Math.floor(Math.random() * numbers.length);
+      genPassword += numbers[random];
+    }
+
+  // user is able to confirm whether or not they'd like special characters in their PW
+  var specialChar = Boolean(confirm("Would you like your password to contain special characters?"));
+  if (specialChar) {
+    for (var i = 0; i < count; i++) {
+      var random = Math.floor(Math.random() * special.length);
+      genPassword += special[random];
+    }
   }
 
   console.log(genPassword);
 
   return genPassword;
+}
 }
 
 // Write password to the #password input
